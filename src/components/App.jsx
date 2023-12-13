64fc00af1688216603cd78a7f526350894d29a15
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-12-13 11:46:36 +0200

    module 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Product } from './Product';
+
+export const App = () => (
+  <div>
+    <h1>Best selling products</h1>
+    <Product
+      imgUrl="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>"
+      name="Tacos With Lime"
+      price={10.993}
+    />
+    <Product
+      imgUrl="<https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640>"
+      name="Fries and Burger"
+      price={14.293}
+    />
+  </div>
+);
---
+++ src/components/Product.jsx
@@ -0,0 +1,8 @@
+export const Product = ({ imgUrl, name, price }) => (
+  <div>
+    <img src={imgUrl} alt={name} width="640" />
+    <h2>{name}</h2>
+    <p>Price: {price}$</p>
+    <button type="button">Add to cart</button>
+  </div>
+);
